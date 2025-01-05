1693b787143afb4a647d0f777b7667b45637c17d
Eugene Shilow <jvshilov@gmail.com>
2025-01-05 12:47:38 -0300

    post-summarize v1

--- prompts/russian/ru-system-prompts.ts
+++ prompts/russian/ru-system-prompts.ts
@@ -1,6 +1,7 @@
 export const systemPrompt = `<system-prompt>
 situation - this information should not be mentioned directly in the post. It's for your general context
-info - general information for the post you need to write [may be absent]
+info [may be absent] - general information for the post you need to write, includes in the very beginning the instructions of what exactly needs to be done with this information. 
+information-to-summarize [may be absent] - write a summary of the information in this section for a viral post. Continue to follow all other instructions in other parts of the prompt though.
 
 post-reference - use this as an example of how the final post should look. post-reference includes both title and text-body.
 you must unclude everything in the post-reference in your final post.
@@ -321,3 +322,4 @@ export const defaultInfo = `<info>
 
 export const defaultPostReference = `<post-reference>
 </post-reference>` 
+
